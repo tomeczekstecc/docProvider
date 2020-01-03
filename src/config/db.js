705f11e57fc51b9712e4a1e239e33dc0f5f3fb2a
Tomasz Steć <tomeczekstecc@gmail.com>
2020-01-03 22:21:38 +0100

    MONGO_URI

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,19 @@
-const mongoose = require('mongoose')
-
-
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-
   try {
-    const conn = await mongoose.connect('MONGO_URI', {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useFindAndModify: true,
       useCreateIndex: true,
       useUnifiedTopology: true
-    })
+    });
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
-    process.exit(1)
+    process.exit(1);
   }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
